4df7d842ba633a3a45cb27c542b37a5a1b0d3346
황인성 <dlstjd2029@naver.com>
2021-08-18 12:37:08 +0900

    Create NavBar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,47 @@
+<template>
+    <nav class="nav02">
+        <ul>
+            <li><a href="#">Notice</a></li>
+            <li>
+                <a href="#">Menu Title</a>
+                <ul>
+                    <li><a href="#">Sub Title</a></li>
+                    <li><a href="#">Sub Title</a></li>
+                    <li><a href="#">Sub Title</a></li>
+                </ul>
+            </li>
+            <li>
+                <a href="#">Menu Title</a>
+                <ul>
+                    <li><a href="#">Sub Title</a></li>
+                    <li><a href="#">Sub Title</a></li>
+                    <li><a href="#">Sub Title</a></li>
+                </ul>
+            </li>
+            <li><a href="#">Menu Title</a>
+                <ul>
+                    <li><a href="#">Sub Title</a></li>
+                    <li><a href="#">Sub Title</a></li>
+                    <li><a href="#">Sub Title</a></li>
+                </ul>
+            </li>
+            <li><a href="#">Menu Title</a>
+                <ul>
+                    <li><a href="#">Sub Title</a></li>
+                    <li><a href="#">Sub Title</a></li>
+                    <li><a href="#">Sub Title</a></li>
+                </ul>
+            </li>
+        </ul>
+    </nav>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style>
+@import url(../css/NavBar.css);
+</style>
